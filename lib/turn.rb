bfc9a586169dcc120c4f93728ff410ef09c4205d
Phuong Vu <phuong.pmv@gmail.com>
2017-10-09 15:37:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip()
   index = input_to_index(input)
-  if valid_move?(board)
+  if valid_move?(board, index)
     move(board, index, player)
     display_board(board)
   else
